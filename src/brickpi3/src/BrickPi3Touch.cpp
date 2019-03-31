5f44fd6e104100dbc9ffead6baa9f74df9c1f097
Henrik Larsen <henrik7264@outlook.com>
2019-03-31 23:41:45 +0200

    Removed debug and updated error in ros_robot.yaml

--- src/brickpi3/src/BrickPi3Touch.cpp
+++ src/brickpi3/src/BrickPi3Touch.cpp
@@ -18,11 +18,7 @@ BrickPi3Touch::BrickPi3Touch(const std::string& aName, const std::string& aFrame
     freq = aFrequency;
     seqNo = 0;
 
-    std::cout << __FUNCTION__ << ", " << __LINE__  << ", "  << port << std::endl;
-
     brickPi3.set_sensor_type(port, SENSOR_TYPE_TOUCH_NXT);
-
-    std::cout << __FUNCTION__ << ", " << __LINE__ << std::endl;
 }
 
 
@@ -30,14 +26,10 @@ void BrickPi3Touch::schedulerCB()
 {
     std::lock_guard<std::mutex> guard(mutex);
 
-    std::cout << __FUNCTION__ << ", " << __LINE__ << std::endl;
-
     sensor_touch_t sensorTouch;
     int rc = brickPi3.get_sensor(port, &sensorTouch);
     if (rc == 0) {
         ROS_DEBUG("Touch sensor: pressed %s", (sensorTouch.pressed) ? "True" : "False");
-
-        std::cout << __FUNCTION__ << ", " << __LINE__ << std::endl;
 
         brickpi3_msgs::Contact contact;
         contact.header.frame_id = frameId;
@@ -45,11 +37,7 @@ void BrickPi3Touch::schedulerCB()
         contact.header.seq = seqNo++;
         contact.contact = sensorTouch.pressed;
 
-        std::cout << __FUNCTION__ << ", " << __LINE__ << std::endl;
-
         touchPublisher.publish(contact);
-
-        std::cout << __FUNCTION__ << ", " << __LINE__ << std::endl;
     }
     else {
         ROS_ERROR("BrickPi3 failed to read Color sensor %s, rc %d", name.c_str(), rc);
